2693909e32233c67bf96be25eab98e255ddeca02
Vipul Chodankar <vipul72c@gmail.com>
2019-08-23 13:10:58 +0530

    Initial Setup

---
+++ pilot-bot.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log('Connected as ' + client.user.tag)   
+})
+
+// Enter Bot Token Here
+client.login('')
