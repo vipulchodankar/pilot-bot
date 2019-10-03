6d15f27733b9d1cc57ffcf8ad7f58fde80c08e5d
Vipul Chodankar <vipul72c@gmail.com>
2019-10-04 01:15:32 +0530

    Cleaned up

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
-# pilot-bot
-<h1>A Discord test bot.</h1>
+# Pilot Bot
+> A Discord test bot.
 
-<h3>Some commands:</h3>
-<ul>
-    <li>;help [arguments]</li>
-    <li>;ping<br><em>Returns 'pong!'</em></li>
-    <li>;[hey,hi,hello,yo,sup]<br><em>Greets the user</em></li>
-    <li>;add [arguments]<br><em>Adds the arguments</em></li>
-    <li>;multiply [arguments]<br><em>Multiplies the arguments</em></li>
-    <li>;[image,img,picture,pic]<br><em>Fetches a random image</em></li>
-</ul>
+### Getting Started ###
+
+1. Replace myToken value with your token.
+2. Run `npm install` to install dependancies.
+3. - Run `npm run start` to start the bot.
+   - Run `npm run dev` to start the bot in dev mode.
+
+### Some commands: ###
+
+- `;help [arguments]`
+
+- `;ping` 
+    Returns 'pong!'
+
+- `;[hey,hi,hello,yo,sup]`
+    Greets the user 
+
+- `;add [arguments]`
+    Adds the arguments 
+
+- `;multiply [arguments]`
+    Multiplies the arguments 
+
+- `;[image,img,picture,pic]`
+    Fetches a random image 
+
+### Discord ###
+[What is Discord?](https://discordapp.com) <br>
+[Discord.js](https://discord.js.org)
--- pilot-bot.js
+++ pilot-bot.js
@@ -1,136 +1,170 @@
 // import axios from 'axios'
-const axios = require('axios')
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const axios = require("axios");
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('Connected as ' + client.user.tag)
+client.on("ready", () => {
+  console.log("Connected as " + client.user.tag);
 
-    client.user.setActivity("YOU", {type: "WATCHING"});
+  client.user.setActivity("YOU", { type: "WATCHING" });
 
-    client.guilds.forEach((guild) => {
-        console.log(guild.name)
-        guild.channels.forEach((channel) => {
-            console.log(` - ${channel.name} ${channel.type} ${channel.id}`)
-        })
-        // General Channel ID: 614358411697586206
-    })
+  client.guilds.forEach(guild => {
+    console.log(guild.name);
+    guild.channels.forEach(channel => {
+      console.log(` - ${channel.name} ${channel.type} ${channel.id}`);
+    });
+    // General Channel ID: 614358411697586206
+  });
 
-    let generalChannel = client.channels.get('614358411697586206')
-    const attachment = new Discord.Attachment('https://vetstreet.brightspotcdn.com/dims4/default/60b6ebe/2147483647/thumbnail/590x420/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F1b%2F10%2F187d796e44db9b3262b45d2e5aac%2Fdog-makes-eye-contact-thinkstockphotos-511375254-590lc031616.jpg')
+  let generalChannel = client.channels.get("614358411697586206");
+  const attachment = new Discord.Attachment(
+    "https://vetstreet.brightspotcdn.com/dims4/default/60b6ebe/2147483647/thumbnail/590x420/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F1b%2F10%2F187d796e44db9b3262b45d2e5aac%2Fdog-makes-eye-contact-thinkstockphotos-511375254-590lc031616.jpg"
+  );
 
-    generalChannel.send("I'm online peeps!")
-    // generalChannel.send(attachment)
-})
+  generalChannel.send("I'm online peeps!");
+  // generalChannel.send(attachment)
+});
 
-client.on('message', (recievedMessage) => {
-    if(recievedMessage.author === client.user){
-        return
-    }
-
-    // recievedMessage.channel.send(recievedMessage.author.toString() + ", you said: " + recievedMessage.content)
-    // recievedMessage.react('👍')
-
-    if (recievedMessage.content.startsWith(";")){
-        processCommand(recievedMessage)
-    }
-})
-
-function processCommand(recievedMessage){
-    let fullCommand = recievedMessage.content.substr(1)
-    let splitCommand = fullCommand.split(" ")
-    let primaryCommand = splitCommand[0]
-    let arguments = splitCommand.slice(1)
-
-    switch(primaryCommand){
-        case 'help': helpCommand(arguments, recievedMessage); break;
-
-        case 'sup':
-        case 'yo':
-        case 'hello':
-        case 'hey':
-        case 'hi': hiCommand(recievedMessage); break;
-
-        case 'ping': pingCommand(recievedMessage); break;
-
-        case 'multiply': multiplyCommand(arguments, recievedMessage); break;
-
-        case 'add': addCommand(arguments, recievedMessage); break;
-
-        case 'image':
-        case 'img':
-        case 'picture':
-        case 'pic': getPic(arguments, recievedMessage); break;
-
-        default: recievedMessage.channel.send("Sorry, i didn't understand what you said. Try again?"); break;
-    }
-}
-
-function helpCommand(arguments, recievedMessage){
-    if(arguments.length === 0){
-        recievedMessage.channel.send(recievedMessage.author.toString() + ", I'm not sure what you need help with. Try ';help [topic]")
-    } else {
-        recievedMessage.channel.send(recievedMessage.author.toString() + ", it looks like you need help with " + arguments)
-    }
-}
-
-function pingCommand(recievedMessage){
-    recievedMessage.channel.send("pong!")
-}
-
-function hiCommand(recievedMessage){
-    let greeting = ['Hi', 'Hello', 'Yo', 'Sup?', 'Hey']
-    recievedMessage.channel.send(greeting[Math.floor(Math.random()*5)] + " " + recievedMessage.author.toString())
-}
-
-function multiplyCommand(arguments, recievedMessage){
-    if(arguments.length < 2){
-        recievedMessage.channel.send('Not enough arguments. Try `;multiply 2 10`')
-        return
-    }
-
-    let product = 1
-    arguments.forEach((value) => {
-        product = product * parseFloat(value)
-    })
-
-    recievedMessage.channel.send("The product of " + arguments + " is " + product.toString())
-}
-
-function addCommand(arguments, recievedMessage){
-    if(arguments.length < 2){
-        recievedMessage.channel.send('Not enough arguments. Try `;add 2 10`')
-        return
-    }
-
-    let sum = 0
-    arguments.forEach((value) => {
-        sum = sum + parseFloat(value)
-    })
-
-    recievedMessage.channel.send("The sum of " + arguments + " is " + sum.toString())
-}
-
-const getImage = async (parameter) => {
-    try {
-      return await axios.get('http://www.splashbase.co/api/v1/images/' + parameter )
-    } catch (error) {
-      console.error(error)
-    }
+client.on("message", recievedMessage => {
+  if (recievedMessage.author === client.user) {
+    return;
   }
 
-const getPic = async(arguments, recievedMessage) => {
-    
+  // recievedMessage.channel.send(recievedMessage.author.toString() + ", you said: " + recievedMessage.content)
+  // recievedMessage.react('👍')
 
-    if(arguments.length === 0){
-        const image = await getImage('random')
-        if(image.data.url){
-            recievedMessage.channel.send("Here's a random image for you!")
-            const randomMedia = new Discord.Attachment(image.data.url)
-            recievedMessage.channel.send(randomMedia)
-        }
-    }
+  if (recievedMessage.content.startsWith(";")) {
+    processCommand(recievedMessage);
+  }
+});
+
+function processCommand(recievedMessage) {
+  let fullCommand = recievedMessage.content.substr(1);
+  let splitCommand = fullCommand.split(" ");
+  let primaryCommand = splitCommand[0];
+  let arguments = splitCommand.slice(1);
+
+  switch (primaryCommand) {
+    case "help":
+      helpCommand(arguments, recievedMessage);
+      break;
+
+    case "sup":
+    case "yo":
+    case "hello":
+    case "hey":
+    case "hi":
+      hiCommand(recievedMessage);
+      break;
+
+    case "ping":
+      pingCommand(recievedMessage);
+      break;
+
+    case "multiply":
+      multiplyCommand(arguments, recievedMessage);
+      break;
+
+    case "add":
+      addCommand(arguments, recievedMessage);
+      break;
+
+    case "image":
+    case "img":
+    case "picture":
+    case "pic":
+      getPic(arguments, recievedMessage);
+      break;
+
+    default:
+      recievedMessage.channel.send(
+        "Sorry, i didn't understand what you said. Try again?"
+      );
+      break;
+  }
 }
 
+function helpCommand(arguments, recievedMessage) {
+  if (arguments.length === 0) {
+    recievedMessage.channel.send(
+      recievedMessage.author.toString() +
+        ", I'm not sure what you need help with. Try ';help [topic]"
+    );
+  } else {
+    recievedMessage.channel.send(
+      recievedMessage.author.toString() +
+        ", it looks like you need help with " +
+        arguments
+    );
+  }
+}
+
+function pingCommand(recievedMessage) {
+  recievedMessage.channel.send("pong!");
+}
+
+function hiCommand(recievedMessage) {
+  let greeting = ["Hi", "Hello", "Yo", "Sup?", "Hey"];
+  recievedMessage.channel.send(
+    greeting[Math.floor(Math.random() * 5)] +
+      " " +
+      recievedMessage.author.toString()
+  );
+}
+
+function multiplyCommand(arguments, recievedMessage) {
+  if (arguments.length < 2) {
+    recievedMessage.channel.send("Not enough arguments. Try `;multiply 2 10`");
+    return;
+  }
+
+  let product = 1;
+  arguments.forEach(value => {
+    product = product * parseFloat(value);
+  });
+
+  recievedMessage.channel.send(
+    "The product of " + arguments + " is " + product.toString()
+  );
+}
+
+function addCommand(arguments, recievedMessage) {
+  if (arguments.length < 2) {
+    recievedMessage.channel.send("Not enough arguments. Try `;add 2 10`");
+    return;
+  }
+
+  let sum = 0;
+  arguments.forEach(value => {
+    sum = sum + parseFloat(value);
+  });
+
+  recievedMessage.channel.send(
+    "The sum of " + arguments + " is " + sum.toString()
+  );
+}
+
+const getImage = async parameter => {
+  try {
+    return await axios.get(
+      "http://www.splashbase.co/api/v1/images/" + parameter
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getPic = async (arguments, recievedMessage) => {
+  if (arguments.length === 0) {
+    const image = await getImage("random");
+    if (image.data.url) {
+      recievedMessage.channel.send("Here's a random image for you!");
+      const randomMedia = new Discord.Attachment(image.data.url);
+      recievedMessage.channel.send(randomMedia);
+    }
+  }
+};
+
 // Enter Bot Token Here
-client.login('')
+const myToken = " ";
+client.login(myToken);
